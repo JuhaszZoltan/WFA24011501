ada64728d0727af4a6c676a1e6224fab64310ed1
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-01-15 08:37:01 +0100

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WFA24011501
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.ForestGreen;
+            button1.Location = new Point(12, 9);
+            button1.Name = "button1";
+            button1.Size = new Size(448, 53);
+            button1.TabIndex = 0;
+            button1.Text = "ADD NEW RENT";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(496, 33);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(264, 29);
+            comboBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 81);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(748, 325);
+            dataGridView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(496, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(166, 21);
+            label1.TabIndex = 3;
+            label1.Text = "filter for this type only:";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(772, 418);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4);
+            Name = "MainForm";
+            Text = "Kobold Bikers 2023";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private ComboBox comboBox1;
+        private DataGridView dataGridView1;
+        private Label label1;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,10 @@
+namespace WFA24011501
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
